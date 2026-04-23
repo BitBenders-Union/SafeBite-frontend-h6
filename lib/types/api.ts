f6015d16507790285db1eb05711d4db8dfc3ee30
D.Paaske <102860689+Denpas404@users.noreply.github.com>
2026-04-23 06:11:41 +0200

    types
    move api types into "type" for reuse

---
+++ lib/types/api.ts
@@ -0,0 +1,11 @@
+// /lib/types/api.ts
+
+export type PageResponse<T> = {
+    data: T[];
+    page: number;
+    pageSize: number;
+    totalCount: number;
+    totalPages: number;
+    hasPreviousPage: boolean;
+    hasNextPage: boolean;
+};
