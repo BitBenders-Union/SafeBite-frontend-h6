1921df0244e0c7706d387709cee06c7a48f5d95e
D.Paaske <102860689+Denpas404@users.noreply.github.com>
2026-04-23 06:10:31 +0200

    allergy and user

---
+++ services/api/allergyApi.ts
@@ -0,0 +1,26 @@
+// /services/apiServices/allergyApi.ts
+
+import { Allergy, CreateAllergyRequest, CreateAllergyResponse, UpdateAllergyRequest } from "@/lib/types/allergy";
+import { PageResponse } from "@/lib/types/api";
+import { apiClient } from "./apiClient";
+
+export async function getAllergies(): Promise<Allergy[]> {
+    const response = await apiClient.get<PageResponse<Allergy>>("/api/Allergy");
+    return response.data.data;
+}
+
+export async function createAllergy(
+    allergyData: CreateAllergyRequest
+): Promise<CreateAllergyResponse> {
+    const response = await apiClient.post<CreateAllergyResponse>(
+        "/api/Allergy?Page=1&PageSize=50",
+        allergyData
+    );
+    return response.data;
+}
+
+export async function updateAllergy(
+    updatedAllergyData: UpdateAllergyRequest
+): Promise<void> {
+    await apiClient.put("/api/Allergy", updatedAllergyData);
+}
