e5f4ef52a02cdb58f74d085fd219dc033854935b
D.Paaske <102860689+Denpas404@users.noreply.github.com>
2026-05-07 14:45:14 +0200

    updatede from dev

--- services/api/adminUserManagementApi.ts
+++ services/api/adminUserManagementApi.ts
@@ -3,43 +3,43 @@ import { UserList, UserRole } from "@/lib/types/user";
 import { apiClient } from "./apiClient";
 
 export async function getAllUsersAndRoles(): Promise<UserList[]> {
-  const response = await apiClient.get<PageResponse<UserList>>(
-    "/api/User/UserManagement/GetAllUsersWithRoles",
-  );
-  return response.data.data;
+    const response = await apiClient.get<PageResponse<UserList>>(
+        "/api/User/UserManagement/GetAllUsersWithRoles",
+    );
+    return response.data.data;
 }
 
 export async function setRole(userRole: UserRole) {
-  const response = await apiClient.post(
-    "/api/User/UserManagement/SetRole",
-    userRole,
-  );
-  return response;
+    const response = await apiClient.post(
+        "/api/User/UserManagement/SetRole",
+        userRole,
+    );
+    return response;
 }
 
 export async function removeRole(userRole: UserRole) {
-  const response = await apiClient.post(
-    "/api/User/UserManagement/RemoveRole",
-    userRole,
-  );
-  return response;
+    const response = await apiClient.post(
+        "/api/User/UserManagement/RemoveRole",
+        userRole,
+    );
+    return response;
 }
 
 export async function activateUser(userID: string) {
-  const response = await apiClient.post(
-    `/api/User/UserManagement/${userID}/Activate`,
-  );
-  return response;
+    const response = await apiClient.post(
+        `/api/User/UserManagement/${userID}/Activate`,
+    );
+    return response;
 }
 
 export async function deactivateUser(userID: string) {
-  const response = await apiClient.post(
-    `/api/User/UserManagement/${userID}/Deactivate`,
-  );
-  return response;
+    const response = await apiClient.post(
+        `/api/User/UserManagement/${userID}/Deactivate`,
+    );
+    return response;
 }
 
 export async function getTotalUsers(): Promise<number> {
-  const response = await apiClient.get<number>(`/api/User/total-count`);
-  return response.data;
+    const response = await apiClient.get<number>(`/api/User/total-count`);
+    return response.data;
 }
