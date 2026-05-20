96be84504b736bde59e29ac8f48e267f48fb0976
D.Paaske <102860689+Denpas404@users.noreply.github.com>
2026-05-20 22:13:04 +0200

    Update AuthCard.tsx

--- components/Shared/AuthCard.tsx
+++ components/Shared/AuthCard.tsx
@@ -22,7 +22,7 @@ export function AuthCard({ children }: Props) {
             {/* logo */}
             <View className="mb-6 items-center justify-center">
                 <Image
-                    source={require("@/assets/images/safebiteDark.png")}
+                    source={require("@/assets/images/SafeBiteDark.png")}
                     style={{
                         width: 160,
                         height: 160,
